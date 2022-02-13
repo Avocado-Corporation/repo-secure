53afafbb84c943d8ad92bc4e9f5edf6c0e2d4e5d
Rudy Sarmiento <rudy750@gmail.com>
2022-02-13 10:06:06 -0500

    testing reading action

--- src/webhook-lambda/src/index.ts
+++ src/webhook-lambda/src/index.ts
@@ -1,17 +1,24 @@
 /* eslint-disable import/no-import-module-exports */
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
 import { Webhooks } from '@octokit/webhooks';
+import { RepositoryEvent } from '@octokit/webhooks-types';
 
 exports.handler = async (event: APIGatewayProxyEventV2)
 : Promise<APIGatewayProxyResultV2> => {
   // eslint-disable-next-line no-console
-
-  const verify = await new Webhooks({
-    secret: process.env?.GH_REPOSECURE_WEBHOOK || '',
-  }).verify(event.body as any, event.headers['x-hub-signature-256'] as string);
-
   console.log('My Events', event);
-  console.log('verified', verify);
+  const verified = await new Webhooks({
+    secret: process.env?.GH_REPOSECURE_WEBHOOK || '',
+  }).verify(event.body as any, event.headers['X-Hub-Signature'] as string);
+
+  if (!verified) return { statusCode: 500 };
+
+  const ghEvent = event.headers['X-GitHub-Event'] as string;
+  const body = JSON.parse(event.body as any) as RepositoryEvent;
+
+
+
+  console.log('verified', verified);
   // ...
-  return { body: '', statusCode: 200 };
+  return { body: body.action, statusCode: 200 };
 };
