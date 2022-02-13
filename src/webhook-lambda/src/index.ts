5c9727e42c237170facbf2d56bf673c6a592cd90
Rudy Sarmiento <rudy750@gmail.com>
2022-02-13 00:28:33 -0500

    added verification of webhook

--- src/webhook-lambda/src/index.ts
+++ src/webhook-lambda/src/index.ts
@@ -1,10 +1,17 @@
 /* eslint-disable import/no-import-module-exports */
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
+import { Webhooks } from '@octokit/webhooks';
 
 exports.handler = async (event: APIGatewayProxyEventV2)
 : Promise<APIGatewayProxyResultV2> => {
   // eslint-disable-next-line no-console
+
+  const verify = new Webhooks({
+    secret: process.env?.GH_REPOSECURE_WEBHOOK || '',
+  }).verify(event.body as any, event.headers['x-hub-signature-256'] as string);
+
   console.log('My Events', event);
+  console.log('verified', verify);
   // ...
   return { body: '', statusCode: 200 };
 };
